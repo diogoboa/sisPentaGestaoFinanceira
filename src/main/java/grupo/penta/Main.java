75c9f41eb64c697655de4311e423821c61f7e314
Diogo Boaventura <diogo.boaventura.ufms@gmail.com>
2023-04-20 15:08:58 -0300

    criacao do novo projeto

--- src/main/java/grupo/penta/Main.java
+++ src/main/java/grupo/penta/Main.java
@@ -1,5 +1,13 @@
 package grupo.penta;
 
+
+
+@SpringBootApplication
+@EntityScan({"penta.sisPenta"})
+@EnableJpaRepositories({"penta.sisPenta"})
+@ComponentScan({"penta.sisPenta", "penta.sisPenta.Controller", "penta.sisPenta.Configuration", "penta.sisPenta.Reports"})
+@EnableCaching
+@Configuration
 public class Main {
     public static void main(String[] args) {
 
